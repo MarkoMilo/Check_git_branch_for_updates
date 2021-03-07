cb48429f72daf0becb0404eebe60ccca7d8afb3d
markomilosa <markomi@veriests.com>
2021-03-07 20:05:46 +0100

    update main branch

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import time
+from check_git_for_updates import do_something
 
+
+def main():
+    do_something()
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(3600)  # check repository every 1 hour
